2e52a756cecd2e52c73d796e3bbabf030c8eac25
Jacob Nishimura <jacob-nishimura@uiowa.edu>
2017-09-17 01:17:21 -0500

    Added receipt for total order

---
+++ app/src/main/java/com/example/receiptstash/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.example.receiptstash;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by jacobnishimura on 9/16/17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+}
