68d42cf8f728edf023d5c10a0534f0d14dd0bbb9
Jacob Nishimura <jacob-nishimura@uiowa.edu>
2017-09-17 02:25:32 -0500

    Implemented receipts for individual transactions, and added icon

---
+++ app/src/main/java/com/example/receiptstash/IndividualReceipt.java
@@ -0,0 +1,13 @@
+package com.example.receiptstash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualReceipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_receipt);
+    }
+}
